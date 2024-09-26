4560c9d3c3a58a466a70cbfa4b3406884ae67801
PCcasa <josedavids123@live.com>
2024-09-26 00:15:27 -0500

    funcionalidad descargar

--- src/main/java/com/assisantsProject/asistantProject/controladores/HomeController.java
+++ src/main/java/com/assisantsProject/asistantProject/controladores/HomeController.java
@@ -4,7 +4,6 @@
  */
 package com.assisantsProject.asistantProject.controladores;
 
-
 import com.assisantsProject.asistantProject.entidades.Candidato;
 import com.assisantsProject.asistantProject.servicios.CandidatoServicio;
 
@@ -22,12 +21,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  *
@@ -35,60 +36,65 @@ import org.springframework.web.bind.annotation.PathVariable;
  */
 @Controller
 public class HomeController {
-    
+
     @Value("${valor.ruta}")
     private String ruta;
-    
-    
+
     @Autowired
     private CandidatoServicio candidatoServicio;
-    
+
     @GetMapping("/")
-    public String home(Model model){
+    public String home(Model model) {
         return "home";
     }
-    
+
     @GetMapping("/listaCandidatos")
-    public String listaCandidatos(Model model){
+    public String listaCandidatos(Model model) {
         List<Candidato> listaCandidatos = candidatoServicio.listarCandidatos();
         model.addAttribute("listaCandidatos", listaCandidatos);
-   
+
         return "listaCandidatos";
     }
+
     @GetMapping("/listaCandidatos/{nombre}/descargar-archivos")
     public ResponseEntity<Resource> descargarArchivosCandidatos(@PathVariable String nombre) throws IOException {
-        Path directorioCandidato = Paths.get(this.ruta + "archivos/"+nombre);
-        
-        String nombreZip = nombre + "_archivos.zip";
-        Path rutaZip = Files.createTempFile(nombreZip, ".zip");
-        try(ZipOutputStream zipOut = new ZipOutputStream(Files.newOutputStream(rutaZip))) {
+        // Normalizar el nombre para usar en la ruta
+        String nombreNormalizado = nombre.replaceAll("[^a-zA-Z0-9\\-\\.\\_]", "_"); // Reemplazar caracteres no válidos
+        Path directorioCandidato = Paths.get(this.ruta + "archivos/" + nombreNormalizado);
+
+        // Verificar si el directorio existe
+        if (!Files.exists(directorioCandidato) || !Files.isDirectory(directorioCandidato)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "El directorio no existe");
+        }
+
+        String nombreZip = nombreNormalizado + "_archivos.zip"; // Usar el nombre normalizado
+        Path rutaZip = Files.createTempFile(null, ".zip");
+
+        try (ZipOutputStream zipOut = new ZipOutputStream(Files.newOutputStream(rutaZip))) {
             Files.walk(directorioCandidato)
                     .filter(Files::isRegularFile)
                     .forEach(archivo -> {
-                    try (InputStream in = Files.newInputStream(archivo)) {
-                        ZipEntry zipEntry = new ZipEntry(directorioCandidato.relativize(archivo).toString());
-                        zipOut.putNextEntry(zipEntry);
-                        byte[] buffer = new byte[2000];
-                        int len;
-                        while ((len = in.read(buffer)) > 0) {
-                           zipOut.write(buffer, 0 ,len);
-                            
+                        try (InputStream in = Files.newInputStream(archivo)) {
+                            ZipEntry zipEntry = new ZipEntry(directorioCandidato.relativize(archivo).toString());
+                            zipOut.putNextEntry(zipEntry);
+                            byte[] buffer = new byte[4096];
+                            int len;
+                            while ((len = in.read(buffer)) > 0) {
+                                zipOut.write(buffer, 0, len);
+                            }
+                            zipOut.closeEntry();
+                        } catch (IOException e) {
+                            e.printStackTrace(); // Manejar excepciones adecuadamente
                         }
-                        zipOut.closeEntry();
-                    
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                    
-                    
-                    
                     });
-        } 
+        }
+
         Resource recurso = new UrlResource(rutaZip.toUri());
+
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + nombreZip + "\"")
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .body(recurso);
     }
-    
+
 }
--- src/main/resources/static/js/funciones.js
+++ src/main/resources/static/js/funciones.js
@@ -9,28 +9,5 @@ function carga_ajax_get(ruta, valor, div){
     });
     return false;
 }
-function descargarArchivos(nombreCandidato){
-    fetch("/listaCandidatos/"+nombreCandidato+"/descargar-archivos", {
-        method:"GET"
-    })
-            .then(response => {
-                if(!response.ok){
-                    throw new Error("Error al descargar archivos");
-                }
-                return response.blob();
-    })
-            .then(blob =>{
-                var url = window.URL.createObjectUrl(blob);
-                var a = document.createElement('a');
-                a.href=url;
-                a.download = nombreCandidato + "_archivos.zip";
-                document.body.appendChild(a);
-                a.click();
-                a.remove();
-                
-    })
-            .catch(error => {
-                console.error("Error: ", error);
-    });
-}
+
 
